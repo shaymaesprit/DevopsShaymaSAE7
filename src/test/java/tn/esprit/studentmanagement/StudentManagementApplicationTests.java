3a43301ea7787a4cd62729f5ba1780566a52d798
shaymaesprit <tlili.chayma@esprit.tn>
2025-12-22 20:02:26 +0100

    puch

--- src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
+++ src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
@@ -1,13 +1,13 @@
 package tn.esprit.studentmanagement;
-
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.junit.jupiter.api.Test;
 
 @SpringBootTest
+@ActiveProfiles("test")  // Utilise le profil H2
 class StudentManagementApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
